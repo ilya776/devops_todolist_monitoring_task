d03c707e549535e23a792542efab7f45dddb6420
Illia Bilous <krabbys1111@gmail.com>
2025-06-26 20:02:23 +0300

    Add Prometheus client to requirements and metrics endpoint to URLs

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
+from metrics import metrics_view
 from api import views
 
 router = DefaultRouter()
@@ -13,4 +13,5 @@ urlpatterns = [
     path("", include(router.urls)),
     path("health", views.health, name="health"),  # API Health check endpoint
     path("ready", views.ready, name="ready"),
+    path("metrics", metrics_view),
 ]
